8ad5f48bd8a63ec6ca147eadd862f8b393164329
madokatext <40591232+madokatext@users.noreply.github.com>
2026-07-19 04:19:02 +0800

    fix: prioritize two-finger player gestures

--- lib/common/widgets/gesture/player_gesture_recognizer.dart
+++ lib/common/widgets/gesture/player_gesture_recognizer.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/gestures.dart'
-    show ScaleGestureRecognizer, RecognizerCallback, GestureRecognizer;
+    show
+        GestureDisposition,
+        GestureRecognizer,
+        PointerCancelEvent,
+        PointerDownEvent,
+        PointerEvent,
+        PointerUpEvent,
+        RecognizerCallback,
+        ScaleGestureRecognizer;
 
 mixin PlayerGestureMixin on GestureRecognizer {
   bool isPosAllowed = true;
@@ -25,4 +33,39 @@ class PlayerScaleGestureRecognizer extends ScaleGestureRecognizer
     super.trackpadScrollCausesScale,
     super.trackpadScrollToScaleFactor,
   });
+
+  final Set<int> _activePointers = <int>{};
+
+  @override
+  void addAllowedPointer(PointerDownEvent event) {
+    super.addAllowedPointer(event);
+    _activePointers.add(event.pointer);
+
+    // A vertical pinch competes with the surrounding video's vertical
+    // Scrollable. Claim the gesture as soon as the second finger lands,
+    // before that Scrollable can interpret either finger as a drag.
+    if (_activePointers.length >= 2) {
+      resolve(GestureDisposition.accepted);
+    }
+  }
+
+  @override
+  void handleEvent(PointerEvent event) {
+    super.handleEvent(event);
+    if (event is PointerUpEvent || event is PointerCancelEvent) {
+      _activePointers.remove(event.pointer);
+    }
+  }
+
+  @override
+  void rejectGesture(int pointer) {
+    _activePointers.remove(pointer);
+    super.rejectGesture(pointer);
+  }
+
+  @override
+  void dispose() {
+    _activePointers.clear();
+    super.dispose();
+  }
 }
