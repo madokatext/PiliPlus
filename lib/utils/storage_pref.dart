c956f87ea0c41dc66d749f70cca0f13493d64ee6
madokatext <40591232+madokatext@users.noreply.github.com>
2026-07-19 15:36:13 +0800

    feat: add danmaku font selection

--- lib/utils/storage_pref.dart
+++ lib/utils/storage_pref.dart
@@ -1078,6 +1078,9 @@ abstract final class Pref {
   static double get danmakuOpacity =>
       _setting.get(SettingBoxKey.danmakuOpacity, defaultValue: 1.0);
 
+  static String get danmakuFontFamily =>
+      _setting.get(SettingBoxKey.danmakuFontFamily, defaultValue: '');
+
   static double get danmakuFontScale => _setting.get(
     SettingBoxKey.danmakuFontScale,
     defaultValue: PlatformUtils.isMobile ? 1.0 : 1.4,
