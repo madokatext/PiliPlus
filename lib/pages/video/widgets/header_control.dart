ec4d1677d8802734636dde49f42c6f90fe3ca679
madokatext <40591232+madokatext@users.noreply.github.com>
2026-07-20 17:10:01 +0800

    Update header_control.dart

--- lib/pages/video/widgets/header_control.dart
+++ lib/pages/video/widgets/header_control.dart
@@ -1781,7 +1781,7 @@ class HeaderControlState extends State<HeaderControl>
       primary: false,
       automaticallyImplyLeading: false,
       toolbarHeight:
-          (showFSActionItem ? 88.0 : 44.0) * thicknessScale,
+          (showFSActionItem ? 70.0 : 35.0) * thicknessScale,
       flexibleSpace: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
