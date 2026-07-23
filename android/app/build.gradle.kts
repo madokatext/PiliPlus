4a6d468dd40883c9bafc932bbd6545a6ce5df119
madokatext <40591232+madokatext@users.noreply.github.com>
2026-07-24 00:24:36 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = "2.1a"
+        versionName = "2.1b"
     }
 
     packagingOptions.jniLibs.useLegacyPackaging = true
