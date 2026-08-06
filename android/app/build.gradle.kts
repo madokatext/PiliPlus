11de5eaef778ae7abcee5b0507033c412ae73a38
madokatext <40591232+madokatext@users.noreply.github.com>
2026-08-06 17:42:46 +0800

    更新版本号为 2.1h

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = "2.1g"
+        versionName = "2.1h"
     }
 
     packagingOptions.jniLibs.useLegacyPackaging = true
--- lib/build_config.dart
+++ lib/build_config.dart
@@ -5,7 +5,7 @@ abstract final class BuildConfig {
   );
   static const String versionName = String.fromEnvironment(
     'pili.name',
-    defaultValue: '2.1g',
+    defaultValue: '2.1h',
   );
 
   static const int buildTime = int.fromEnvironment('pili.time');
