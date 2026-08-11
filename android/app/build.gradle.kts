ef015d1fd94f10cbf935258b7ff894e9961443f4
madokatext <40591232+madokatext@users.noreply.github.com>
2026-08-12 02:21:15 +0800

    更新版本号为 2.1i

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = "2.1h"
+        versionName = "2.1i"
     }
 
     packagingOptions.jniLibs.useLegacyPackaging = true
